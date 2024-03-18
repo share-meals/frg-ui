06dae4f6936cc9b1db4172ccb2eb805f3f01e4b0
Jon Chin <jon@sharebyteaching.org>
2024-03-18 05:50:13 -0400

    moves Map center when MapProvider is given new center

---
+++ src/Map/Map.DynamicMap.stories.tsx
@@ -0,0 +1,56 @@
+import {
+  IonLabel,
+  IonSegment,
+  IonSegmentButton,
+} from '@ionic/react';
+import {
+  Map,
+  MapProvider
+} from './Map';
+import type {
+  Meta,
+  StoryObj
+} from '@storybook/react';
+import {useState} from 'react';
+
+const parks = {
+  prospect: {lat: 40.66019478378121, lng: -73.96947593651886},
+  central: {lat: 40.78249320246721, lng: -73.96539607078506}
+};
+
+const meta: Meta<typeof Map> = {
+  component: Map,
+  render: (props) => {
+    const [park, setPark] = useState<['prospect', 'central']>('prospect');
+    
+    return <>
+      <IonSegment value={park} onIonChange={(event) => {setPark(event.detail.value);}}>
+	<IonSegmentButton value='prospect'>
+	  <IonLabel>Prospect Park</IonLabel>
+	</IonSegmentButton>
+	<IonSegmentButton value='central'>
+	  <IonLabel>Central Park</IonLabel>
+	</IonSegmentButton>
+      </IonSegment>
+      [{parks[park].lat}, {parks[park].lng}]
+      <div style={{height: '50vh', width: '100vw'}}>
+	<MapProvider
+	  center={parks[park]}
+	  layers={[]}
+	  maxZoom={20}
+	  minZoom={14}>
+	  <Map
+	    {...props}
+	  />
+	</MapProvider>
+      </div>
+    </>;
+  },
+  title: 'Components/Map'
+}
+
+export default meta;
+type Story = StoryObj<typeof Map>;
+
+export const DynamicCenter: Story = {};
+
