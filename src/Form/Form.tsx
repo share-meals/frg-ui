782663569fa1b1573306918cfc114a99c2dd3b0a
Jon Chin <jon@sharemeals.org>
2025-02-04 11:41:37 -0500

    added form component

--- src/Form/Form.tsx
+++ src/Form/Form.tsx
@@ -21,10 +21,9 @@ import {MultipleCheckbox} from '../MultipleCheckbox';
 import ReactMarkdown from 'react-markdown';
 import {InlineSelect} from '../InlineSelect';
 import {StateButton} from '../StateButton';
-import {
-  useState
-} from 'react';
-//import {zodResolver} from '@hookform/resolvers/zod';
+import {useCallback, useState} from 'react';
+import {z} from 'zod';
+import {zodResolver} from '@hookform/resolvers/zod';
 
 const MultipleChoiceQuestion: React.FC<Omit<MultipleChoiceQuestionType, 'type'>> = ({
   maxSelections,
@@ -34,38 +33,46 @@ const MultipleChoiceQuestion: React.FC<Omit<MultipleChoiceQuestionType, 'type'>>
 }) => {
   const {
     control,
-    formState: {disabled}
+    formState: {disabled},
+    getValues,
   } = useFormContext();
   return <>
-    <ReactMarkdown children={text} />
-    <MultipleCheckbox
-      control={control}
-      disabled={disabled}
-      justify='start'
-      labelPlacement='end'
-      maxSelections={maxSelections}
-      name={name}
-      options={options}
-      wrapper={({children}) => <IonItem>{children}</IonItem>}
-    />
+  <ReactMarkdown children={text} />
+  <MultipleCheckbox
+  control={control}
+  defaultValue={getValues(name)}
+  disabled={disabled}
+  justify='start'
+  labelPlacement='end'
+  maxSelections={maxSelections}
+  name={name}
+  options={options}
+  wrapper={({children}) => <IonItem>{children}</IonItem>}
+  />
   </>;
 };
 
 const SingleChoiceQuestion: React.FC<Omit<SingleChoiceQuestionType, 'type'>> = ({
+  name,
   text,
   ...props
 }) => {
   const {
     control,
-    formState: {disabled}
+    formState: {disabled},
+    getValues,
+    //    getFieldState,
+    //    trigger
   } = useFormContext();
   return <>
     <ReactMarkdown children={text} />
     <InlineSelect
       control={control}
+      defaultValue={getValues(name)}
       disabled={disabled}
       justify='start'
       labelPlacement='end'
+      name={name}
       wrapper={({children}) => <IonItem>{children}</IonItem>}
       {...props}
     />
@@ -94,19 +101,110 @@ const TextModule: React.FC<TextModuleType> = ({text}) => {
 }
 
 export const Page: React.FC<{
+  data: any,
+  isLoading?: boolean,
   json: PageType,
-  pageNumber: number
+  onSubmit: (lastPageData: any) => void,
+  pageNumber: number,
+  setData: any,
+  setPageNumber: any,
+  totalPages: number,
 }> = ({
+  data,
+  isLoading = false,
   json,
-  pageNumber
+  onSubmit: onSubmitProps,
+  pageNumber,
+  setData,
+  setPageNumber,
+  totalPages,
 }) => {
-  return json.map(({module, ...props}, index: number) => {
-    return <div key={`${pageNumber}-${index}`}>
-      {module === 'text' && <TextModule {...props as TextModuleType} />}
-      {module === 'question' && <QuestionModule {...props as QuestionModuleType} />}
-    </div>;
+  const schema = z.object(
+    Object.fromEntries(
+      json
+	.filter((m) => m.module === 'question')
+      // @ts-ignore
+	.map((q) => [q.name, generateZodSchema(q)])
+    )
+  );
+  
+  const methods = useForm<z.infer<typeof schema>>({
+    disabled: isLoading,
+    mode: 'onChange',
+    resolver: zodResolver(schema)
   });
+
+  const onSubmit = methods.handleSubmit(
+    useCallback((pageData) => {
+      setData({
+	...data,
+	...pageData,
+      });
+      if(pageNumber + 1 < totalPages){
+	setPageNumber(pageNumber + 1);
+      }else{
+	onSubmitProps(pageData);
+      }
+    }, [
+      data,
+      pageNumber,
+      setData,
+      setPageNumber,
+      totalPages,
+    ]
+  ));
+  return<FormProvider {...methods}>
+  <form onSubmit={onSubmit}>
+    {
+      json.map(({module, ...props}, index: number) => {
+	return <div key={`${pageNumber}-${index}`}>
+	  {module === 'text' && <TextModule {...props as TextModuleType} />}
+	  {module === 'question' && <QuestionModule {...props as QuestionModuleType} />}
+	</div>;
+      }
+      )}
+    <IonGrid>
+      <IonRow>
+	<IonCol>
+	  {pageNumber > 0 &&
+	   <StateButton
+	     isLoading={isLoading}
+	     onClick={() => {
+	       setPageNumber(pageNumber - 1);
+	     }}>
+	     Previous Page
+	   </StateButton>
+	  }
+	</IonCol>
+	<IonCol className='ion-text-right'>
+	  <StateButton
+	    isLoading={isLoading}
+	    type='submit'>
+	    {pageNumber + 1 < totalPages ? 'Next Page' : 'Submit'}
+	  </StateButton>
+	</IonCol>
+      </IonRow>
+    </IonGrid>
+  </form>
+  </FormProvider>;
 }
+
+const generateZodSchema = (q: QuestionModuleType) => {
+  let schema;
+  if(q.type === 'multipleChoice'){
+    schema = z.array(z.string());
+    if(q.required){
+      schema = schema.nonempty();
+    }
+  }
+  if(q.type === 'singleChoice'){
+    schema = z.string();
+    if(q.required){
+      schema = schema.nonempty();
+    }
+  }
+  return schema;
+};
 
 export const Form: React.FC<{
   isLoading: boolean,
@@ -115,51 +213,30 @@ export const Form: React.FC<{
 }> = ({
   isLoading,
   json,
-  onSubmit,
+  onSubmit: onSubmitProps,
 }) => {
   const [pageNumber, setPageNumber] = useState<number>(0);
-  const methods = useForm({
-    disabled: isLoading
-  });
-  return <>
-    <FormProvider {...methods}>
-      <form onSubmit={methods.handleSubmit(onSubmit)}>
-	<Page
-	  json={json.pages[pageNumber]}
-	  pageNumber={pageNumber}
-	/>
-	<IonGrid>
-	  <IonRow>
-	    <IonCol>
-	      {pageNumber > 0 &&
-	       <StateButton
-		 isLoading={isLoading}
-		 onClick={() => {
-		   setPageNumber(pageNumber - 1);
-		 }}>
-		 Previous Page
-	       </StateButton>
-	      }
-	    </IonCol>
-	    <IonCol className='ion-text-right'>
-	      {pageNumber + 1 < json.pages.length &&
-	       <StateButton
-		 isLoading={isLoading}
-		 onClick={() => {
-		   setPageNumber(pageNumber + 1);
-		 }}>
-		 Next Page
-	       </StateButton>
-	      }
-	      {pageNumber + 1 === json.pages.length &&
-	       <StateButton isLoading={isLoading} type='submit'>
-		 Submit
-	       </StateButton>
-	      }
-	    </IonCol>
-	  </IonRow>
-	</IonGrid>
-      </form>
-    </FormProvider>
-  </>;
+  const [data, setData] = useState<{[key: string]: any}>({});
+  const onSubmit = useCallback((lastPageData: any) => {
+    // need to merge with lastPageData since last submit will not have enough time to update
+    const finalData = {
+      ...data,
+      ...lastPageData
+    }
+    setData(finalData);
+    onSubmitProps(finalData);
+  }, [data, onSubmitProps]);
+  return <Page
+	   json={json.pages[pageNumber]}
+	   totalPages={json.pages.length}
+	   {...{
+	     data,
+	     isLoading,
+	     onSubmit,
+	     pageNumber,
+	     setData,
+	     setPageNumber,
+	   }}
+  />;
 }
+
